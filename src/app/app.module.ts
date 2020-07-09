4bd2c91e8777e27f009687cd00fb2ab5b8368f30
AnthonyBassplayer <creepcreep@yandex.ru>
2020-07-09 19:23:59 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListComponent } from './list/list.component';
+import { Item1Component } from './item1/item1.component';
+import { Item2Component } from './item2/item2.component';
+import { Item3Component } from './item3/item3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ListComponent,
+    Item1Component,
+    Item2Component,
+    Item3Component
   ],
   imports: [
     BrowserModule
